fcb3979f8289d5b7a8ab8118ba2df2f95ada98dd
Carly Magiera <carlymagiera@Carlys-MacBook-Pro.local>
2022-07-03 19:32:41 -0700

    deliverable 2 complete

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+
+--- Deliverable 1
 -- retirment titles table
 SELECT e.emp_no,
 	e.first_name,
@@ -28,3 +30,27 @@ INTO retiring_titles
 FROM unique_titles as u
 GROUP BY title
 ORDER BY COUNT(u.emp_no) DESC;
+
+---Devliverable 2
+-- mentorship eligibility table
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_emp as de
+    ON e.emp_no = de.emp_no
+LEFT JOIN titles as t 
+    ON e.emp_no = t.emp_no
+WHERE de.to_date = ('9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
+
+
+
+
+
